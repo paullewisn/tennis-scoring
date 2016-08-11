40438d406cc83ba1e441380286b019e080c3bd96
paullewisn <paul-lewis.nunn@comparethemarket.com>
2016-08-11 11:34:02 +0100

    first commit with initial test

---
+++ TennisScoring.Tests/Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisScoring.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Scorer scorer;
+
+        [SetUp]
+        public void Init()
+        {
+            scorer = new Scorer();
+        }
+
+        [Test]
+        public void ScoreProducesResult()
+        {
+            var result = scorer.GetScore(15, 15);
+            Assert.That(result, Is.Not.Empty);
+        }
+    }
+}
---
+++ TennisScoring/Scorer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisScoring
+{
+    public class Scorer
+    {
+        public string GetScore(int player1score, int player2score)
+        {
+            return "score1 - score2";
+        }
+    }
+}
